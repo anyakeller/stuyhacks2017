62a516a75c32f12c7b2991f62bf31f7f04ed28ad
Daniel Monteagudo <dmonte3210@gmail.com>
2017-05-27 14:34:15 -0400

    updated devlog

---
+++ utils/database.py
@@ -0,0 +1,36 @@
+import hashlib
+import sqlite3
+
+# check_login takes a parameter for username and a hashed password
+# returns a boolean indicating whether the username-password pair is valid
+def check_login(username, hashed_pw):
+    path = "data/data.db"
+    database = sqlite3.connect(path)
+    curse = database.cursor()
+    command = "SELECT username, passhash FROM users WHERE username =='" + username + "';"
+    reference = curse.execute(command)
+    
+    # cross references data in users table
+    for record in reference:
+        if record[1] == hashed_pw:
+            print "Yay"
+            database.close()
+            return True
+        else:
+            print "Nay"
+            database.close()
+            return False
+        return False
+    
+# create_user takes a username and password
+# returns whether user creation was successful
+# should always be true unless a username exists in db
+def create_user(username, hashed_pass):
+    path = "data/data.db"
+    database = sqlite3.connect(path)
+    curse = database.cursor()
+    insert = "INSERT INTO users VALUES (%d, '%s', '%s', '%s')" % (hash(username), username, hashed_pass, 0)
+    curse.execute(insert)
+    database.commit()
+    database.close()
+    return True   
